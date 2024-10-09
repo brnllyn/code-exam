8af40e80313c4d211926bda19051e3649ab655e9
brnslzr <brianallynsalazar@gmail.com>
2024-10-09 15:07:10 +0800

    Initial commit

---
+++ src/main/java/com/example/coding_exam/CodingExamApplication.java
@@ -0,0 +1,13 @@
+package com.example.coding_exam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodingExamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodingExamApplication.class, args);
+	}
+
+}
